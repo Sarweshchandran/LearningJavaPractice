23fcbf2adb85773c824a76bb7bf2f886e82771b8
Sarweshchandran <sarweshchandran@gmail.com>
2024-06-13 17:05:59 +0530

    Updated Calculator

---
+++ Inheritance/Calculator.java
@@ -0,0 +1,31 @@
+package Practice;
+
+class Calculator{
+    public int add(int n1, int n2){
+        return (n1+n2);
+    }
+
+    public int add(int n1, int n2, int n3){
+        return (n1+n2+n3);
+    }
+
+    public int sub(int n1, int n2){
+        return (n1-n2);
+    }
+
+    public int sub(int n1, int n2, int n3){
+        return (n1-n2-n3);
+    }
+
+    public int multiply(int n1, int n2){
+        return (n1*n2);
+    }
+
+    public int multiply(int n1, int n2, int n3){
+        return (n1*n2*n3);
+    }
+
+    public int divide(int n1, int n2){
+        return (n1/n2);
+    }
+}
--- Inheritance/Demo.java
+++ Inheritance/Demo.java
@@ -1,34 +1,5 @@
 package Practice;
 
-class Calculator{
-    public int add(int n1, int n2){
-        return (n1+n2);
-    }
-
-    public int add(int n1, int n2, int n3){
-        return (n1+n2+n3);
-    }
-
-    public int sub(int n1, int n2){
-        return (n1-n2);
-    }
-
-    public int sub(int n1, int n2, int n3){
-        return (n1-n2-n3);
-    }
-
-    public int multiply(int n1, int n2){
-        return (n1*n2);
-    }
-
-    public int multiply(int n1, int n2, int n3){
-        return (n1*n2*n3);
-    }
-
-    public int divide(int n1, int n2){
-        return (n1/n2);
-    }
-}
 public class Demo extends Calculator{
     public static void main(String[] args) {
         Demo obj = new Demo();
